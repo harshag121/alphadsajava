d0fa412db3c27c5cc93092d4100c4922949a1cca
harshag121 <harshavardhang222@gmail.com>
2024-02-16 19:37:19 +0530

    Array.java

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,7 +1,9 @@
-public  class Array{
-	
-    public static void main(String[] args){
-        System.out.println("Hello"+"world");
-        }
+
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      Scanner scan = new Scanner(System.in);
     }
-}
+  }
